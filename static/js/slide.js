58e4c33d5ad2e80d407d67bb81980951649b8d9c
SU-T <guitaristpeet@gmail.com>
2022-12-13 12:57:46 -0800

    apply absolute positioning to slideshow

--- static/js/slide.js
+++ static/js/slide.js
@@ -1,10 +1,7 @@
 const swiper1 = new Swiper('#swiper1', {
   direction: 'horizontal',
   loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
   autoplay: {
-    delay: 3000
-  }
+    delay: 4500
+  },
 });
