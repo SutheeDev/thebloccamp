234e5447ba7e0171fd70ba567354a241c0494407
SU-T <guitaristpeet@gmail.com>
2022-12-14 13:40:25 -0800

    add about, shows, and contact.html

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/shows')
+def shows():
+    return render_template('shows.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
