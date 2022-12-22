a876e6e883b05f4f6f80e32e52e905acbb348324
SU-T <guitaristpeet@gmail.com>
2022-12-21 17:14:24 -0800

    add comment

--- app.py
+++ app.py
@@ -10,14 +10,21 @@ app = Flask(__name__)
 
 db = cs50.SQL("sqlite:///shows.db")
 
+# Get band info from the database
 allInfo = db.execute("SELECT * FROM shows ORDER BY date")
 for i in range(len(allInfo)):
+    # Get the date column, which is string and alter them into a datetime object
     ymd_str = allInfo[i]['date']
     datetime_obj = datetime.strptime(ymd_str, '%Y-%m-%d').date()
+    # Get date from the datetimeobject
     day_num = datetime_obj.day
+    # Get the day name from the datetimeobject
     day = datetime_obj.strftime('%A')
+    # Get month from the datetimeobject
     month = calendar.month_abbr[datetime_obj.month]
+    # Get year from the datetimeobject
     year = datetime_obj.strftime('%Y')
+    # Append date, day, month, year back to the allInfo dictionary
     allInfo[i]['day_num'] = day_num
     allInfo[i]['day'] = day
     allInfo[i]['month'] = month
@@ -27,6 +34,7 @@ for i in range(len(allInfo)):
 @app.route('/')
 def index():
     fourUpcoming = []
+    # Want to have 4 slides in the slideshow, so append the first four shows from the allInfo to a new list called fourUpcoming
     for i in range(4):
         fourUpcoming.append(allInfo[i])
     return render_template('index.html', allInfo=allInfo, fourUpcoming=fourUpcoming)
@@ -50,12 +58,17 @@ def contact():
 @app.route('/tickets', methods=['GET', 'POST'])
 def tickets():
     if request.method == 'POST':
+        # Passed the 'Get Tickets.eachInfo['id']' in the shows page
+        # So need to extract the band id out of the name attribute here
         for key in request.form:
             if key.startswith('Get Tickets.'):
                 id = key.partition('.')[-1]
+                # Obtain the band info from the database using the extracted id
                 bandInfos = db.execute("SELECT * FROM shows WHERE id = ?", id)
+                # Change date column to the datetime object
                 ymd_str = bandInfos[0]['date']
                 datetime_obj = datetime.strptime(ymd_str, '%Y-%m-%d').date()
+                # Append back to the bandInfos
                 bandInfos[0]['day_num'] = datetime_obj.day
                 bandInfos[0]['day'] = datetime_obj.strftime('%A')
                 bandInfos[0]['month'] = calendar.month_abbr[datetime_obj.month]
@@ -72,20 +85,24 @@ def subscribe():
 @app.route('/subscribed', methods=['POST'])
 def subscribed():
     if request.method == 'POST':
+        # Get information from the form input
         firstname = request.form.get('firstname')
         lastname = request.form.get('lastname')
         email = request.form.get('email-address')
 
+        # Handle error if a user doesn't fill in all fields
         if not firstname or not lastname or not email:
             error_statement = '* All form fields are required'
             return render_template('subscribe.html', error_statement=error_statement)
 
+        # Utilize smtplib library to notify the page admin via email
         message = "You've been subscribed to the Bloc Camp email newsletter"
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login("sutheedevtest@gmail.com", os.environ.get('MY_PASSWORD'))
         server.sendmail("sutheedevtest@gmail.com", email, message)
 
+        # Add subscriber info into the subscribers table
         db.execute("INSERT INTO subscribers (firstname, lastname, email) VALUES (?, ?, ?)",
                    firstname, lastname, email)
         return render_template('subscribed.html', firstname=firstname)
